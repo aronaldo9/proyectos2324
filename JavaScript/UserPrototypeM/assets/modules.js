13e995e4d063312dbd03c63ae6607a8adc8bfc70
aronaldo9 <aaronpesqueirafarina@gmail.com>
2023-10-20 00:05:12 +0200

    acabando userPrototypeM

--- JavaScript/UserPrototypeM/assets/modules.js
+++ JavaScript/UserPrototypeM/assets/modules.js
@@ -28,10 +28,11 @@ export const usuarioLiteral = {
     set dni(newDni) {
         // aquí tenemos que validar que newDni es correcto
         this.validarDni(newDni) ? (this._dni = newDni) : false;
+        
     },
     validarDni(miDni) {
         const letras = "TRWAGMYFPDXBNJZSQVHLCKE";
-        const dniN = parseInt(miDni.slice(0, 8), 10);
+        const dniN = miDni.slice(0, 8);
         const dniL = (miDni.slice(-1)).toUpperCase();
         if(letras.charAt(dniN % 23) === dniL) {
             return true;
@@ -49,8 +50,104 @@ export const usuarioLiteral = {
 
 // uso de herencia por prototipos con FUNCIONES CONSTRUCTORAS.
 
+export function UsuarioFuncional(nombre, edad=0) {
+    this._nombre = nombre;
+    this._edad = edad;
+    this._dni = "";
 
+    // Getter y setter para el nombre
+    Object.defineProperty(this,"nombre", {
+        get : function() {
+            return this._nombre;
+        },
+        set : function(nuevoNombre) {
+            this._nombre = nuevoNombre;
+        },
+    });
+
+    Object.defineProperty(this,"edad", {
+        get : function() {
+            return this._edad;
+        },
+        set : function(nuevaEdad) {
+            if(nuevaEdad  >= 0) {
+                this._edad = nuevaEdad;
+            }else {
+                console.error("La edad debe ser un número positivo");
+            }
+        },
+    });
+
+    Object.defineProperty(this,"dni", {
+        get : function() {
+            return this._dni;
+        },
+        set : function(nuevoDni) {
+            const letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+            const dniN = miDni.slice(0, 8);
+            const letraCalculada = letras[dniN % 23];
+            if(nuevoDni.length === 9 && nuevoDni[8] === letraCalculada) {
+                this.dni = nuevoDni;
+            }
+            else{
+                console.log("El dni no es correcto");
+            }
+
+        },
+    });
+
+}
+
+// puedo añadir las propiedades prototipales a posteriori de la siguiente forma
+UsuarioFuncional.prototype.mostrarInfo = function () {
+    console.log(
+        `Bienvenido ${this.nombre}, usted tiene ${this.edad} años y su Dni es ${this.dni}`
+    );
+};
+
+// para crear un método estático se haría así, y estaría disponible sólo en la función constructora
+UsuarioFuncional.saludo = function () {
+    return "Hola mundo";
+};
 
 
 
 // uso de herencia por prototipos con CLASS.
+export class UsuarioClases {
+    constructor(nombre = "", edad = 0) {
+        this._nombre = nombre;
+        this._edad = edad;
+        this._dni = "";
+    }
+
+    get nombre() {
+        return this._nombre;
+    }
+    set nombre(nuevoNombre) {
+        this._nombre = nuevoNombre;
+    }
+
+    get edad() {
+        return this._edad;
+    }
+    set edad(nuevaEdad) {
+        this._edad = nuevaEdad;
+    }
+
+    get dni() {
+        return this._dni;
+    }
+    set dni(nuevoDni) {
+        this._dni = this.validarDni(nuevoDni);
+    }
+
+    validarDni(miDni) {
+        const letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+        const dniN = parseInt(miDni.slice(0, 8), 10);
+        const dniL = (miDni.slice(-1)).toUpperCase();
+        if(letras.charAt(dniN % 23) === dniL) {
+            return true;
+        }
+        return false;
+    }
+}
--- JavaScript/UserPrototypeM/script.js
+++ JavaScript/UserPrototypeM/script.js
@@ -2,7 +2,8 @@
  * Crear una app web que a través de un formulario me permita recoger los datos del nombre, edad y dni.
  * Cuando pulsemos el botón de insertar usuario automáticamente creará usando la herencia por prototipos, 
  * un usuario de tipo usuarioLiteral, y guardará en un array dichos objetos, almacenándolos en el 
- * localStorage a través de la clave datosUsuarios.
+ * localStorage a través de la clave datosUsuarios. Si el dni no está repetido en la clave de LS donde se 
+ * almacene mi array de objetos, entonces se almacenará.
  */
 
 // ------- Imports ----------
@@ -13,6 +14,27 @@ let arrayUsers = [];
 
 
 // ------- Funciones --------------
+
+function insertUsersLocalStorage(key,obj) {
+    if(localStorage.getItem(key)) { // verifica que exista el objeto en LS
+        arrayUsers = JSON.parse(localStorage.getItem(key));
+
+        if(arrayUsers.some( user => user._dni === obj.dni)) { // verifica si se repite el dni
+            document.getElementById('mensajeError').style.display = 'block';
+            setTimeout(() => {document.getElementById('mensajeError').style.display = 'none'}, 5000);
+        }
+        else {
+            console.log("object");
+            arrayUsers.push(obj);
+        }
+
+    }else {
+        arrayUsers.push(obj);
+    }
+
+    localStorage.setItem(key, JSON.stringify(arrayUsers));
+}
+
 function handlerInsertarUsuario(e) {
     e.preventDefault();
     // const formularioUsuarios = document.getElementById('formularioUsuarios');
@@ -22,36 +44,50 @@ function handlerInsertarUsuario(e) {
 
     // para instanciar o crear un objeto cuyo prototipo sea UsuarioLiteral???
     // const objetoHijo = Object.create(nombre del objeto padre)
+
     let newUser = Object.create(Usuario);
     newUser.nombre = usernameFrm;
     newUser.edad = edadFrm;
     newUser.dni = dniFrm;
 
-    let mensaje = "";
-    if (newUser.dni === dniFrm) {
-        mensaje = "DNI Insertado Correctamente";
-        // Agregar el usuario al array
-        arrayUsers.push(newUser);
-        console.log(newUser.mostrarInfo());
-        // Guardar en localStorage
-        localStorage.setItem("datosUsuarios", JSON.stringify(arrayUsers));
-    } else {
-        mensaje = "Error al insertar el DNI";
+    if ((newUser.dni)) {
+        document.getElementById('mensajeExito').style.display = 'block';
+        setTimeout(() => {document.getElementById('mensajeExito').style.display = 'none'}, 5000);
+        
+
+        insertUsersLocalStorage('datosUsuarios', newUser);
+    }
+    else{
+        document.getElementById('mensajeError').style.display = 'block';
+        setTimeout(() => {document.getElementById('mensajeError').style.display = 'none'}, 5000);
+        // document.getElementById('mensajeExito').style.display = 'none';
     }
 
-    const mensajeElement = document.getElementById(newUser.dni === dniFrm ? "mensajeExito" : "mensajeError");
-        mensajeElement.textContent = mensaje;
-        mensajeElement.style.display = "block";
-
-    // Mostrar el mensaje después de 5 segundos
-    setTimeout(() => {
-        mensajeElement.remove();
-    }, 5000); 
+    // Limpiar el formulario
+    document.getElementById('username').value = '';
+    document.getElementById('edad').value = '';
+    document.getElementById('dni').value = '';
+        
 }
 
 function init() {
     document.getElementById("insertarUsuario").addEventListener("click", handlerInsertarUsuario);
 }
+
+function chargeUsers() {
+    const users = JSON.parse(localStorage.getItem('datosUsuarios'));
+    if (users) {
+        const infoUsers = users.map(user => `Nombre de usuario: ${user._nombre}, Edad: ${user._edad}, Dni: ${user._dni}`).join('\n');
+
+        const infoTextArea = document.getElementById('infoUsuarios');
+        infoTextArea.value = infoUsers;
+    } else {
+        console.log('No hay usuarios en LocalStorage.');
+    }
+}
+
+const cargar = document.getElementById('cargar');
+cargar.addEventListener('click', chargeUsers);
 
 
 // ------- Inicio de la Aplicación ----------
